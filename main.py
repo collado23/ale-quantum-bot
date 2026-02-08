ba1357f33ec9c34ed0e470a6b597f63075bf41cf
collado23 <collado23aaa@gmail.com>
2026-02-08 10:34:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import Ale2
 
 if __name__ == "__main__":
-    # Este comando despierta el servidor web y el motor de trading en Ale2.py
-    print("🚀 Gladiador Quantum: Despertando el sistema modular...")
     Ale2.ejecutar_sistema()
